b1c5e27472568656e53ac3e05f270c59cb2a149c
Yash <yash123456789.yg@gmail.com>
2024-07-05 22:10:19 +0530

    update

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,26 @@
+import { Grid, GridItem } from "@chakra-ui/react"
 import DashboardLayout from "../../components/DashboardLayout"
 import PortfolioSection from "./component/PortfolioSection"
+import PriceSection from "./component/PriceSection"
 
 
 const Dashboard = ({}) => {
   return (
     <DashboardLayout title="DashBoard">
-        <div>
-           <PortfolioSection />
-        </div>
+        <Grid
+        gridTemplateColumns={{
+          base: "repeat(2, 1fr)",
+          md: "repeat(2, 1fr)",
+        }}
+        gap="6"
+        >
+          <GridItem colSpan={2}>
+            <PortfolioSection />
+          </GridItem>
+          <GridItem colSpan={1}>
+            <PriceSection />
+          </GridItem>
+        </Grid>
     </DashboardLayout>
   )
 }
---
+++ src/pages/Dashboard/component/PriceSection.jsx
@@ -0,0 +1,78 @@
+import { Button, Flex, HStack, Icon, Image, Stack, Tab, TabList, TabPanel, TabPanels, Tabs, Tag, Text } from "@chakra-ui/react"
+import { CustomCard } from "../../../chakra/CustomCard"
+import { BsArrowUpRight } from "react-icons/bs"
+import { AiFillPlusCircle, AiOutlineMinusCircle } from "react-icons/ai"
+
+
+const PriceSection = () => {
+
+    const timestamps = ["7:15pm", "7:30pm", "8:00pm", "9:00pm", "10:00pm",]
+  return (
+    <CustomCard>
+        <Flex justify="space-between" align="start">
+            <Stack>
+                <HStack color="black.80">
+                    <Text fontSize="sm">Current Price</Text>
+
+                </HStack>
+                <HStack spacing={2}  
+                align={{
+                    base:"flex-start",
+                    sm:"center",
+                }}
+                flexDir={{
+                    base:"column",
+                    sm  :"row",
+                }}>
+                    <HStack>
+                        <Text textStyle="h2" fontWeight="medium">22.39401000</Text>
+                    </HStack>
+                    <HStack fontWeight="medium" color="green.500">
+                        <Icon as={BsArrowUpRight} />
+                        <Text fontSize="sm" fontWeight="medium">22%</Text>
+                    </HStack>
+
+                </HStack>
+            </Stack> 
+            <HStack>
+                <Button 
+                leftIcon={
+                    <Icon as={AiFillPlusCircle} />
+                }>Buy
+                </Button>
+                <Button             
+                leftIcon={
+                    <Icon as={AiOutlineMinusCircle} />
+                }>Sell
+                </Button>
+            </HStack>
+        </Flex>
+        <Tabs variant='soft-rounded' colorScheme='green'>
+           <Flex justify="end">
+           <TabList >
+                <Tab>Tab 1</Tab>
+                <Tab>Tab 2</Tab>
+            </TabList>
+           </Flex>
+            <TabPanels>
+                <TabPanel>
+                <Image w="100%" src="/Graph.png" mt="48px" />
+                <HStack justify="space-between">
+                    {
+                        timestamps.map((timestamps) => (
+                        <Text key={timestamps} fontSize="sm" color="black.80">{timestamps}</Text>
+                    ))
+                    }
+                </HStack>
+                </TabPanel>
+                <TabPanel>
+                <p>two!</p>
+                </TabPanel>
+            </TabPanels>
+        </Tabs>
+        
+    </CustomCard>   
+  )
+}
+
+export default PriceSection
